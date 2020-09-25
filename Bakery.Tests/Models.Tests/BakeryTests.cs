01720d68429f8825ba39a1f8476c8e88eafb782a
Michael Watts <wattsjmichael@gmail.com>
2020-09-25 08:21:16 -0700

    Ran restore command to create files

--- Bakery.Tests/Models.Tests/BakeryTests.cs
+++ Bakery.Tests/Models.Tests/BakeryTests.cs
@@ -1,3 +1,12 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bread.Models;
+
+namespace Bakery.TestTools
+{
+  [TestClass]
+  public class BreadTests
+  {
+    //Tests go here
+  }
+}
